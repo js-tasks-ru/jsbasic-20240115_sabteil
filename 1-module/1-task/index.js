3c65da832056cb35513d37f916b4ffa7f6202405
Arthur Ivanov <sabteilcat@gmail.com>
2024-01-24 02:13:31 +0300

    Решение задачи 1.1 "Функция которая использует для расчёта цикл for.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,22 @@
+// Sabteil (Артур Иванов 20240115)
+
 function factorial(n) {
-  // ваш код...
+  let answer = n; // Первый множитель. Будет возвращать факториал.
+  let dropValue = n; // Второй множитель, который выполяет роль уменьшения множителя
+
+  if (answer == 0) { // Проверка на исключение !0 = 1;
+      return 1;
+  } else {
+    for (let countToStop = n ; countToStop != 1 && countToStop <= n; --countToStop) { //Цикл с счётчиком
+      dropValue--; // (n - 1)
+      answer = answer * dropValue; // n * ( n - 1 )
+      // console.log(answer); // Тестовый вывод
+    }
+  }
+  return answer; // После завершения цикла возвращает полученный факториал
 }
+
+// console.log(`Тест: ${factorial()}`); // Тестовый ввод/вывод
+
+
+
