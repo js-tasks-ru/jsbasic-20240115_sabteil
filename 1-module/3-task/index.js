9a4a6be17da41405220b7172b170ba18240c9548
Arthur Ivanov <sabteilcat@gmail.com>
2024-01-26 03:14:17 +0300

    Добавил решение задач 2-5

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,15 @@
+//Sabteil (Иванов Артур)
 function ucFirst(str) {
-  // ваш код...
+
+  if(str == false) { //Проверка на пустую строку, если true, то возвращает пустую строку
+
+    return "";
+
+  } else {
+
+    let answer = str[0].toUpperCase() + str.slice(1); // Делает первую букву в ввёрхнем регистре и конкатинирует со строкой без первой буквы 
+    // console.log(`Тест: ${answer}`); // Тестовый вывод
+    return answer; 
+
+  }
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,21 @@
+// Sabteil(Артур Иванов)
+
 function checkSpam(str) {
-  // ваш код...
+
+  str = str.toLowerCase(); // Приводит все буквы в тексте к нижнему регистру
+ 
+
+  if (str.includes("1xbet") || str.includes("xxx")) { // Проверяет на запрещённые слова
+
+    // console.log("False"); // Тестовый вывод
+    return true;
+
+  } else {
+
+    // console.log("True"); // Тестовый вывод
+    return false;
+
+  }
 }
+
+// console.log(`Вывод: ${checkSpam("1XBeT")}`); // Тестовый ввод/вывод
