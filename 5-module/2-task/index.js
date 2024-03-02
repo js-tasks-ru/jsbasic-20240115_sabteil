639bb8708a572d6ac2fd3e7276e885e55634a7f5
Arthur Ivanov <sabteilcat@gmail.com>
2024-03-02 20:37:58 +0300

    Решения 5-го модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,25 @@
 function toggleText() {
-  // ваш код...
+  // Находим кнопку по классу
+  const buttonOnClick = document.querySelector('.toggle-text-button');
+
+  // Находим элемент текста по его id
+  let divIsHidden = document.querySelector('#text');
+
+  // Назначаем обработчик события click на кнопку
+  return buttonOnClick.onclick = () => { 
+
+    // Получаем текущее значение атрибута hidden (true/false)
+    const isHidden = divIsHidden.getAttribute("hidden") === "true";
+
+    // Если текст не скрыт, скрываем его
+    if (!isHidden) {
+      divIsHidden.setAttribute("hidden", true); 
+    } else { 
+      // Если текст уже скрыт, показываем его
+      divIsHidden.removeAttribute('hidden'); 
+    }
+  }
 }
+
+// Вызываем функцию после полной загрузки страницы
+document.addEventListener('DOMContentLoaded', toggleText);
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,53 @@
 function initCarousel() {
-  // ваш код...
+  
+  let rightClick = document.querySelector('.carousel__arrow.carousel__arrow_right'); // Выбирает правую стрелку
+  //
+  let leftClick = document.querySelector('.carousel__arrow.carousel__arrow_left'); // Выбирает левую стрелку
+  //
+  let carousel = document.querySelector('.carousel__inner');
+
+  leftClick.style.display = 'none'; // По умолчанию скрывает левую стрелку
+  let currentSlide = 0; // Отслеживает текущий слайд
+  let c = 0; // Текущее смещение
+  
+  //Событие при нажатии на правую стрелку
+  rightClick.onclick = () => {
+
+    if (currentSlide != 3) {
+    carousel.style.transform = `translateX(${cheker("right")})` ;
+    currentSlide++ ; // Передает значение текущего слайда
+    if (leftClick.style.display === "none") { leftClick.style.display = '' }; // Возвращает левую стрелку стрелку 
+    if (currentSlide === 3) { rightClick.style.display = "none"  }; // Убирает правую стрелку на последнем слайде
+
+    }
+
+  }
+
+  leftClick.onclick = () => {
+    
+    if (currentSlide != 0) {
+    carousel.style.transform = `translateX(${cheker("left")})`;
+    currentSlide--; // Передает значение текущего слайда
+    if (currentSlide !=3) {rightClick.style.display = ""} // Возвращает правую стрелку
+    if (currentSlide == 0) {leftClick.style.display = 'none'} // Убирает левую стрелку на первом слайде
+
+    }
+
+  }
+
+  function cheker(whatButtonClick) { // функция которая возвращает необходимое смещение для слайда на всю его ширину
+
+    let curentElem = carousel.children[currentSlide];
+    let w = curentElem.offsetWidth;
+
+    if (whatButtonClick === "right") {
+    c += w;
+    return "-" + c + "px";
+    } else {
+    c -= w;
+    return "-" + c + "px";
+    }
+  
+  }
+
 }
