d9b3126acba903ffd0bb4ebc0619f7cb72733894
Arthur Ivanov <sabteilcat@gmail.com>
2024-03-02 23:46:06 +0300

    Должно работать

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -3,23 +3,11 @@ function toggleText() {
   const buttonOnClick = document.querySelector('.toggle-text-button');
 
   // Находим элемент текста по его id
-  let divIsHidden = document.querySelector('#text');
+  let divIsHidden = document.getElementById('text');
 
   // Назначаем обработчик события click на кнопку
-  return buttonOnClick.onclick = () => { 
-
-    // Получаем текущее значение атрибута hidden (true/false)
-    const isHidden = divIsHidden.getAttribute("hidden") === "true";
-
-    // Если текст не скрыт, скрываем его
-    if (!isHidden) {
-      divIsHidden.setAttribute("hidden", true); 
-    } else { 
-      // Если текст уже скрыт, показываем его
-      divIsHidden.removeAttribute('hidden'); 
-    }
-  }
+  buttonOnClick.onclick = () => divIsHidden.hidden = !divIsHidden.hidden;
 }
 
 // Вызываем функцию после полной загрузки страницы
-document.addEventListener('DOMContentLoaded', toggleText);
+document.addEventListener('DOMContentLoaded', toggleText() );
